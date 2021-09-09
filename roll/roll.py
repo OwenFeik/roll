8ab47b492481620d7f922ed875899007a041ea30
OwenFeik <owenhfeik@gmail.com>
2021-09-09 17:56:47 +1000

    start adding subexprs

--- roll/roll.py
+++ roll/roll.py
@@ -1,16 +1,16 @@
 import heapq
+import numbers
 import operator
 import random
 import re
+from sre_constants import MAX_REPEAT
 import typing
-
-numeric = typing.Union[int, float]
 
 
 class Modifier:
     """Stores a value and operation and can apply itself to another value."""
 
-    def __init__(self, val: numeric, opstr: str):
+    def __init__(self, val: numbers.Number, opstr: str):
         self.val = val
         self.opstr = opstr
         self.operation = get_operator(opstr)
@@ -18,7 +18,7 @@ class Modifier:
     def __str__(self):
         return f"{self.opstr} {self.val}"
 
-    def apply(self, val: numeric) -> numeric:
+    def apply(self, val: numbers.Number) -> numbers.Number:
         return self.operation(val, self.val)
 
     @staticmethod
@@ -26,51 +26,187 @@ class Modifier:
         return Modifier(int(string[1:]), string[0])
 
 
-class Roll:
+class Expr:
+    def __init__(self, modifiers: typing.List[Modifier], opstr: str) -> None:
+        self.modifiers = modifiers
+        self.opstr = opstr
+        self.operation = get_operator(opstr)
+
+        self._total = None
+
+    def __str__(self):
+        return self.desc_str()
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}:{str(self)}>"
+
+    @property
+    def total(self) -> numbers.Number:
+        if self._total is None:
+            self.resolve()
+        return self._total
+
+    def full_str(self, pad_desc: int = 0, pad_val: int = 0) -> str:
+        string = (
+            self.desc_str(pad_desc)
+            + "\t"
+            + self.val_str(pad_val)
+            + f"\tTotal: {clean_number(self.total)}"
+        )
+
+        return string
+
+    def desc_str(self, pad_to, include_op):
+        raise NotImplementedError()
+
+    def val_str(self):
+        raise NotImplementedError()
+
+    def roll_str(self):
+        return self.val_str()
+
+    def str_prefix(self, include_opstr: bool = False):
+        return (self.opstr + " ") if include_opstr else ""
+
+    def modf_str(self, include_opstr: bool = False):
+        return self.str_prefix(include_opstr) + (
+            ("{} " + " ".join(str(mod) for mod in self.modifiers))
+            if self.modifiers
+            else "{}"
+        )
+
+    def resolve(self) -> None:
+        self.calculate_total()
+
+    def calculate_total(self):
+        raise NotImplementedError(
+            "Abstract Expr class should not be directly instantiated."
+        )
+
+    def apply(self, val: numbers.Number) -> numbers.Number:
+        return self.operation(val, self.total)
+
+    def apply_mods(self, val: numbers.Number) -> numbers.Number:
+        for mod in self.modifiers:
+            val = mod.apply(val)
+        return val
+
+    def set_operator(self, opstr: str) -> None:
+        self.operation = get_operator(opstr)
+
+    def add_modifier(self, modifier: Modifier) -> None:
+        self.modifiers.append(modifier)
+
+
+class SuperExpr(Expr):
+    def __init__(
+        self,
+        exprs: typing.List["Expr"],
+        modifiers: typing.List[Modifier],
+        opstr: str,
+    ) -> None:
+        super().__init__(modifiers, opstr)
+        self.exprs = exprs
+
+    def desc_str(self, pad_to: int = 0, include_opstr: bool = False):
+        return (
+            self.modf_str(include_opstr)
+            .format(
+                "("
+                + " ".join(
+                    e.desc_str(include_opstr=(i != 0))
+                    for (i, e) in enumerate(self.exprs)
+                )
+                + ")"
+            )
+            .ljust(pad_to)
+        )
+
+    def val_str(self, pad_to: int = 0, include_opstr: bool = False):
+        return (
+            self.str_prefix()
+            + "("
+            + " ".join(
+                e.val_str(include_opstr=(i != 0))
+                for (i, e) in enumerate(self.exprs)
+            )
+            + ")"
+        ).ljust(pad_to)
+
+    def calculate_total(self):
+        self._total = self.apply_mods(calculate_total(self.exprs))
+
+    def clone(self):
+        return SuperExpr(
+            [e.clone() for e in self.exprs], self.modifiers[:], self.opstr
+        )
+
+
+class ConstExpr(Expr):
+    def __init__(
+        self,
+        val: int,
+        modifiers: typing.List[Modifier],
+        opstr: str,
+    ) -> None:
+        super().__init__(modifiers, opstr)
+        self.val = val
+
+    def desc_str(self, pad_to: int = 0, include_opstr: bool = False):
+        return self.modf_str(include_opstr).format(self.val_str()).ljust(pad_to)
+
+    def val_str(self, pad_to: int = 0, include_opstr: bool = False):
+        return (self.str_prefix(include_opstr) + str(self.val)).ljust(pad_to)
+
+    def calculate_total(self):
+        self._total = self.apply_mods(self.val)
+
+    def clone(self):
+        return ConstExpr(self.val, self.modifiers[:], self.opstr)
+
+
+class RollExpr(Expr):
     """Stores information and returns results for a parsed roll."""
 
     MAX_QTY = 1000
 
     def __init__(
         self,
+        modifiers: typing.List[Modifier],
+        opstr: str,
         qty: int,
         die: int,
         adv: bool,
         disadv: bool,
         keep: int,
-        modifiers: typing.List[Modifier],
-        operation: typing.Callable,
     ):
+        super().__init__(modifiers, opstr)
         self.qty = qty
         self.die = die
         self.adv = adv
         self.disadv = disadv
         self.keep = keep
-        self.modifiers = modifiers
-        self.operation = operation
 
-        if self.qty > Roll.MAX_QTY:
+        if self.qty > RollExpr.MAX_QTY:
             raise ValueError("Maximum quantity exceeded.")
 
-        self.resolved = False
-        self._rolls: typing.List[int] = []
-        self._total: numeric = 0
+        self._rolls = None
 
-    def __str__(self):
-        return self.full_str()
+    @property
+    def rolls(self):
+        if self._rolls is None:
+            self.resolve()
+        return self._rolls
 
-    def __repr__(self):
-        return f"<{str(self)}>"
+    def full_str(self, pad_desc: int = 0, pad_val: int = 0) -> str:
+        return (
+            self.desc_str(pad_desc, False)
+            + "\t"
+            + self.roll_str(pad_val)
+            + f"\tTotal: {clean_number(self.total)}"
+        )
 
-    def full_str(self, pad_desc: int = 0, pad_roll: int = 0) -> str:
-        string = self.desc_str(pad_desc) + "\t" + self.roll_str(pad_roll)
-
-        if self.modifiers or len(self.rolls) > 1:
-            string += f"\tTotal: {clean_number(self.total)}"
-
-        return string
-
-    def desc_str(self, pad_to: int = 0) -> str:
+    def desc_str(self, pad_to: int = 0, include_opstr=False) -> str:
         string = "d" + str(self.die)
 
         is_adv_roll = (self.adv or self.disadv) and (self.qty == 2)
@@ -84,17 +220,18 @@ class Roll:
 
         if self.keep >= 0:
             string += f" keep {self.keep}"
-        for mod in self.modifiers:
-            string += " " + str(mod)
 
-        while len(string) < pad_to:
-            string += " "
-
-        return string
+        return self.modf_str(include_opstr).format(string).ljust(pad_to)
 
     def dice_str(self) -> str:
         # Used for saving results in database
         return f"{self.qty}d{self.die}"
+
+    def val_str(self, pad_to: int = 0, include_opstr: bool = False):
+        return (
+            self.str_prefix(include_opstr)
+            + ", ".join([str(r) for r in self.rolls])
+        ).ljust(pad_to)
 
     def roll_str(self, pad_to: int = 0) -> str:
         if len(self.rolls) > 1:
@@ -107,19 +244,7 @@ class Roll:
 
         return string
 
-    @property
-    def total(self) -> numeric:
-        if not self.resolved:
-            self.resolve()
-        return self._total
-
-    @property
-    def rolls(self) -> typing.List[int]:
-        if not self.resolved:
-            self.resolve()
-        return self._rolls
-
-    def calculate_total(self) -> numeric:
+    def calculate_total(self) -> numbers.Number:
         if self.adv:
             total = max(self._rolls)
         elif self.disadv:
@@ -133,42 +258,37 @@ class Roll:
         return self._total
 
     def ensure_rolled(self) -> None:
-        if not self.resolved:
+        if self._rolls is None:
             self._rolls = [random.randint(1, self.die) for _ in range(self.qty)]
-            self.calculate_total()
-            self.resolved = True
 
-    def resolve(self) -> typing.Tuple[typing.List[int], numeric]:
+    def resolve(self) -> None:
         self.ensure_rolled()
-        return self._rolls, self._total
+        self.calculate_total()
 
-    def reroll(self, n: int) -> typing.Tuple[typing.List[int], numeric]:
+    def reroll(self, n: int) -> typing.Tuple[typing.List[int], numbers.Number]:
         for r in heapq.nsmallest(n, self.rolls):
             self._rolls[self._rolls.index(r)] = random.randint(1, self.die)
 
         return self._rolls, self.calculate_total()
 
-    def apply(self, val: numeric) -> numeric:
-        return self.operation(val, self.total)
-
-    def apply_mods(self, val: numeric) -> numeric:
-        for mod in self.modifiers:
-            val = mod.apply(val)
-        return val
-
-    def set_operator(self, opstr: str) -> None:
-        self.operation = get_operator(opstr)
-
-    def add_modifier(self, modifier: Modifier) -> None:
-        self.modifiers.append(modifier)
+    def clone(self):
+        return RollExpr(
+            self.modifiers[:],
+            self.opstr,
+            self.qty,
+            self.die,
+            self.adv,
+            self.disadv,
+            self.keep,
+        )
 
 
 def get_operator(opstr: str) -> typing.Callable:
-    """Return a function related to a given numeric operation string."""
+    """Return a function related to a given numbers.Number operation string."""
 
     # For the inverse division operator (hack), division is simply called
     # with the operands reversed. Pylint assumes that this is a mistake when
-    # it is in fact intended.
+    # it is in fact intended. Same thing for subtraction and inverse.
     #
     # pylint: disable=arguments-out-of-order
 
@@ -178,10 +298,11 @@ def get_operator(opstr: str) -> typing.Callable:
         "*": operator.mul,
         "/": operator.truediv,
         "\\": lambda a, b: operator.truediv(b, a),
+        "~": lambda a, b: operator.sub(b, a),
     }[opstr]
 
 
-def clean_number(num: numeric) -> numeric:
+def clean_number(num: numbers.Number) -> numbers.Number:
     """Round to 2 or less decimal places."""
 
     if num // 1 == num:
@@ -189,133 +310,124 @@ def clean_number(num: numeric) -> numeric:
     return round(num, 2)
 
 
-def parse_mods(modstr: str) -> typing.Tuple[typing.List[Modifier], int]:
+MODS_REGEX = re.compile(r"(?P<op>[+-/*]) *(?P<const>\d+)")
+
+
+def parse_mods(modstr: str) -> typing.List[Modifier]:
     """Parse a string of modifier statements."""
 
+    if not modstr:
+        return []
+
     mods = []
-    keep = -1
-    if not modstr in [None, ""]:
-        modstr = modstr.replace(" ", "") + "k"
-        # Add k to ensure last term is added
-        # won't cause an error in a properly formatted string
+    for mod in re.finditer(MODS_REGEX, modstr):
+        mods.append(Modifier(int(mod.group("const")), mod.group("op")))
 
-        q = ""
-        o = ""
-        for c in modstr:
-            if c in ["k", "+", "-", "*", "/"]:
-                if o == "k" and q:
-                    if keep > 0:
-                        raise ValueError("Multiple keep values provided.")
-                    keep = int(q)
-                elif o:
-                    mods.append(Modifier(int(q), o))
-                o = c
-                q = ""
-            else:
-                q += c
-
-    return mods, keep
+    return mods
 
 
-def get_rolls(string: str, max_qty: int = -1) -> typing.List[Roll]:
-    """Parse a string for roll statements, returning a list of Roll objects."""
+CONST = r"(?P<const>\d+(?= *[+-/*]))"
+OP = r"(?P<op>[+-/*])"
+N = r"(?P<n>\d+(?= +))"
+EXPR = r"\((?P<expr>.*)\)"
+ROLL = r"(?P<qty>\d*)d(?P<die>\d+)(?P<advstr>[ad]*)?(k *(?P<keep>\d+))?"
+C = r"(?P<c>\d+)"
+MODS = r"(?P<mods>( *[+-/*] *\d+(?!d))*(?![\dd]))"
+EXPR_REGEX = re.compile(rf"{CONST}? *({OP}|{N})? *({EXPR}|{ROLL}|{C}) *{MODS}?")
 
-    rolls = []
 
-    # Explanation of groups in the below regex:
-    #
-    # const: a value before the roll followed by an operator. only applies for
-    #   single rolls. e.g. <2> + 2d20
-    # op: the operation to apply to the previous term in the string and the
-    #   roll. e.g. 2 <+> 2d20 <*> d10
-    # n: the number of times to do this roll. must be followed by a space.
-    #   e.g. <2> d20 <6> 4d6k3
-    # qty: the number of dice to roll
-    #   e.g. <2>d10 <8>d6
-    # die: the size of dice to roll
-    #   e.g. 2d<10> 8d<6>
-    # advstr: annotation to indicate whether the roll is at advantage or
-    #   disadvantage. any number of as and ds. e.g. d20<a> d20<addd>
-    # mods: modifiers of the form operator, value or keep x.
-    #   e.g. d20 <+ 8 - 2> 10d6<k8 + 2>
-    #
-    # The expression accepts any number of spaces between terms.
-    regex = re.compile(
-        r"(?P<const>\d+(?= *[+-/*]))? *(?P<op>[+-/*]?)? *(?P<n>\d+"
-        r"(?= +))? *(?P<qty>\d*)d(?P<die>\d+) *(?P<advstr>[ad]*)"
-        r"(?P<mods>( *(k *\d+|[+-/*] *\d+(?!d)))*(?![\dd]))",
-        flags=re.IGNORECASE,
-    )
+def empty_group(match_group):
+    return match_group is None or match_group == ""
 
-    empty_group = lambda g: g in [None, ""]
 
-    count = 0
-    for roll in re.finditer(regex, string.lower()):
+def parse_integer_group(match_group, default=None):
+    if empty_group(match_group):
+        return default
+    else:
+        return int(match_group)
 
-        n = 1 if empty_group(roll.group("n")) else int(roll.group("n"))
-        qty = 1 if empty_group(roll.group("qty")) else int(roll.group("qty"))
 
-        if qty == 0:
-            raise ValueError("I can't roll a zero sided die.")
+def parse_roll_expr(expr) -> RollExpr:
+    qty = parse_integer_group(expr.group("qty"), 1)
 
-        die = int(roll.group("die"))
+    if qty == 0:
+        raise ValueError("Can't roll a zero sided die.")
 
-        advstr = roll.group("advstr")
-        adv = "a" in advstr
-        disadv = "d" in advstr
+    die = parse_integer_group(expr.group("die"))
 
-        # if under advantage and disadvantage, roll is straight
-        if adv and disadv:
-            adv = disadv = False
+    advstr = expr.group("advstr")
+    adv = "a" in advstr
+    disadv = "d" in advstr
 
-        # a roll like d20a implicitly means 2d20a
-        if (adv or disadv) and qty == 1:
-            qty = 2
+    # if under advantage and disadvantage, roll is straight
+    if adv and disadv:
+        adv = disadv = False
 
-        mods, keep = parse_mods(roll.group("mods"))
+    # a roll like d20a implicitly means 2d20a
+    if (adv or disadv) and qty == 1:
+        qty = 2
 
-        opstr = "+" if empty_group(roll.group("op")) else roll.group("op")
-        op = get_operator(opstr)
+    keep = parse_integer_group(expr.group("keep"), -1)
 
-        const = roll.group("const")
-        if not empty_group(const):
+    return qty, die, adv, disadv, keep
+
+
+def parse_expr(string: str, max_qty: int = None):
+    exprs = []
+    for match in re.finditer(EXPR_REGEX, string.lower()):
+
+        const = parse_integer_group(match.group("const"))
+        opstr = "+" if empty_group(match.group("op")) else match.group("op")
+        n = parse_integer_group(match.group("n"), 1)
+        mods = parse_mods(match.group("mods"))
+
+        if not empty_group(match.group("expr")):
+            expr = SuperExpr(parse_expr(match.group("expr")), mods, opstr)
+        elif not empty_group(match.group("c")):
+            expr = ConstExpr(parse_integer_group(match.group("c")), mods, opstr)
+        else:
+            expr = RollExpr(mods, opstr, *parse_roll_expr(match))
+
+        if const is not None:
+            # other operations are commutative
             if opstr == "/":
                 opstr = "\\"
+            elif opstr == "-":
+                opstr = "~"
 
-            special_mods = mods[:]
-
-            special_mods.insert(0, Modifier(int(const), opstr))
-            op = get_operator("+")
-
+            expr.opstr = "+"
+            special = expr.clone()
+            special.modifiers.insert(0, Modifier(const, opstr))
+            exprs.append(special)
             n -= 1
-            rolls.append(Roll(qty, die, adv, disadv, keep, special_mods, op))
 
         for _ in range(n):
-            rolls.append(Roll(qty, die, adv, disadv, keep, mods[:], op))
+            exprs.append(expr.clone())
 
-        count += 1
-        if max_qty > 0 and count == max_qty:
-            break
-
-    return rolls
+    return exprs[:max_qty]
 
 
-def calculate_total(rolls: typing.List[Roll]) -> numeric:
-    """Calculate to total result of a list of Roll objects."""
+def get_rolls(string: str, max_qty: int = None):
+    return parse_expr(string, max_qty)
+
+
+def calculate_total(exprs: typing.List[Expr]) -> numbers.Number:
+    """Calculate to total result of a list of Expr objects."""
 
     total = 0
-    for r in rolls:
-        total = r.apply(total)
+    for expr in exprs:
+        total = expr.apply(total)
 
     return total
 
-def get_result(string: str) -> numeric:
+
+def get_result(string: str) -> numbers.Number:
     """Calculate and return the total of rolls parsed from a string."""
 
     return calculate_total(get_rolls(string))
 
 
-def rolls_string(rolls: typing.List[Roll]) -> str:
+def rolls_string(rolls: typing.List[Expr]) -> str:
     """Return a descriptive string for a list of Roll objects."""
 
     if len(rolls) == 1:
