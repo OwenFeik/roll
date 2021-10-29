4c49d907c0aa344b4051a99e996a3a57a5db603a
OwenFeik <owenhfeik@gmail.com>
2021-10-29 12:43:34 +1100

    add roll_info

--- roll/parser.py
+++ roll/parser.py
@@ -3,20 +3,28 @@ import operator
 import random
 import typing
 
-from roll.roll import OP
-
+# Generic numeric base type
 Number = typing.Union[int, float]
 
 # During lexing this is used for return tuples of the form
 # (finished_token, current_token)
 TokenPair = typing.Tuple[typing.Optional["Token"], typing.Optional["Token"]]
 
+# Type used to represent the result of a given roll
+# (roll_format, results)
+RollInfo = typing.Tuple[str, typing.List[int]]
+
+
 class Token:
     def __init__(self, token=""):
         self.token = token
 
     def __eq__(self, o: object) -> bool:
-        return isinstance(o, Token) and o.token == self.token
+        return (
+            isinstance(o, Token)
+            and type(o) == type(self)
+            and o.token == self.token
+        )
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}<{str(self)}>"
@@ -38,9 +46,11 @@ class Token:
         else:
             return NonTerminal.from_char(c)
 
+
 class Terminal(Token):
     def value(self) -> typing.Any:
         raise NotImplementedError()
+
 
 class Integer(Terminal):
     def __init__(self, token=""):
@@ -61,6 +71,7 @@ class Integer(Terminal):
     def value(self) -> int:
         return int(self.token)
 
+
 class Decimal(Terminal):
     def __init__(self, token):
         super().__init__(token=token)
@@ -75,9 +86,10 @@ class Decimal(Terminal):
             return None, self
         else:
             return self, Token.from_char(c)
-    
+
     def value(self) -> float:
         return float(self.token)
+
 
 class Roll(Terminal):
     SEPERATOR = "d"
@@ -92,7 +104,7 @@ class Roll(Terminal):
     @property
     def qty(self):
         return int(self.token.partition(Roll.SEPERATOR)[0])
-    
+
     @property
     def size(self):
         return int(self.token.partition(Roll.SEPERATOR)[2])
@@ -115,22 +127,19 @@ class NonTerminal(Token):
         else:
             return Operator.from_char(c)
 
+
 class OpenExpr(NonTerminal):
     def __init__(self):
         super().__init__(token="(")
+
 
 class CloseExpr(NonTerminal):
     def __init__(self):
         super().__init__(token=")")
 
+
 class Operator(NonTerminal):
-    PRECEDENCE = {
-        "k": 1,
-        "*": 2,
-        "/": 2,
-        "+": 3,
-        "-": 3
-    }
+    PRECEDENCE = {"k": 1, "*": 2, "/": 2, "+": 3, "-": 3}
     MAX_PRECEDENCE = 3
 
     def __init__(self, token):
@@ -144,6 +153,7 @@ class Operator(NonTerminal):
         else:
             raise ValueError(f"Invalid character: {c}")
 
+
 class Expr:
     def __repr__(self) -> str:
         return f"{type(self).__name__}<{str(self)}>"
@@ -155,21 +165,38 @@ class Expr:
     def value(self) -> Number:
         raise NotImplementedError()
 
-    def roll_info(self) -> typing.List[typing.Tuple[str, int]]:
+    @property
+    def result(self) -> Number:
+        value = self.value
+        if value // 1 == value:
+            return int(value)
+        return round(value, 2)
+
+    def roll_info(self) -> typing.List[RollInfo]:
         return []
+
 
 class TerminalExpr(Expr):
     def __init__(self):
         super().__init__()
 
+    def __eq__(self, o: object) -> bool:
+        return (
+            isinstance(o, TerminalExpr)
+            and type(o) == type(self)
+            and o.value == self.value
+        )
+
+
 class ConstExpr(TerminalExpr):
     def __init__(self, value: Number):
         super().__init__()
         self._value = value
-    
+
     @property
     def value(self) -> Number:
         return self._value
+
 
 class RollExpr(TerminalExpr):
     MAX_QTY = 1000
@@ -182,6 +209,14 @@ class RollExpr(TerminalExpr):
 
         if self.qty > RollExpr.MAX_QTY:
             raise ValueError("Maximum quantity exceeded.")
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            type(o) == type(self)
+            and isinstance(o, RollExpr)
+            and o.qty == self.qty
+            and o.size == self.size
+        )
 
     def __str__(self) -> str:
         return f"{self.qty}{Roll.SEPERATOR}{self.size}"
@@ -198,7 +233,7 @@ class RollExpr(TerminalExpr):
     def value(self) -> Number:
         return sum(self.rolls)
 
-    def roll_info(self) -> typing.List[typing.Tuple[str, int]]:
+    def roll_info(self) -> typing.List[RollInfo]:
         return [(str(self), self.rolls)]
 
     def reroll(self, n: int) -> typing.Tuple[typing.List[int], Number]:
@@ -206,25 +241,48 @@ class RollExpr(TerminalExpr):
         # mypy doesn't know this.
 
         for r in heapq.nsmallest(n, self.rolls):
-            self._rolls[ # type: ignore
-                self._rolls.index(r) # type: ignore
+            self._rolls[  # type: ignore
+                self._rolls.index(r)  # type: ignore
             ] = random.randint(1, self.die)
 
-        return self._rolls, self.calculate_total() # type: ignore 
+        return self._rolls, self.calculate_total()  # type: ignore
 
     @staticmethod
     def from_token(token: Roll):
         return RollExpr(token.qty, token.size)
 
-class OpExpr(Expr):
-    def __init__(
-        self, opstr: str, left: Expr, right: Expr
-    ) -> None:
+
+class NonTerminalExpr(Expr):
+    def __init__(self, exprs: typing.List[Expr]) -> None:
+        super().__init__()
+        self.exprs = exprs
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            type(o) == type(self)
+            and isinstance(o, NonTerminalExpr)
+            and o.exprs == self.exprs
+        )
+
+    def roll_info(self) -> typing.List[RollInfo]:
+        return [info for expr in self.exprs for info in expr.roll_info()]
+
+
+class OpExpr(NonTerminalExpr):
+    def __init__(self, opstr: str, left: Expr, right: Expr) -> None:
+        super().__init__([left, right])
         self.opstr = opstr
         self.left = left
         self.right = right
 
         self.operation = OpExpr.get_operation(opstr)
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            super().__eq__(o)
+            and isinstance(o, OpExpr)
+            and o.opstr == self.opstr
+        )
 
     def __repr__(self) -> str:
         return (
@@ -238,7 +296,10 @@ class OpExpr(Expr):
     @property
     def value(self) -> Number:
         return self.operation(self.left.value, self.right.value)
-    
+
+    def roll_info(self) -> typing.List[RollInfo]:
+        return self.left.roll_info() + self.right.roll_info()
+
     @staticmethod
     def from_token(left: Expr, operator: Operator, right: Expr) -> Expr:
         if operator.token == KeepExpr.CHAR:
@@ -256,13 +317,13 @@ class OpExpr(Expr):
         }[opstr]
 
 
-class KeepExpr(Expr):
+class KeepExpr(NonTerminalExpr):
     CHAR = "k"
 
     def __init__(self, roll, keep) -> None:
-        super().__init__()
-        
         assert isinstance(roll, RollExpr)
+
+        super().__init__([roll, keep])
 
         self.roll = roll
         self.keep = keep
@@ -278,13 +339,12 @@ class KeepExpr(Expr):
 
     @property
     def value(self) -> Number:
-        ... # TODO
+        return sum(heapq.nlargest(int(self.keep.value), self.roll.rolls))
 
 
-class SuperExpr(Expr):
-    def __init__(self, exprs):
-        super().__init__()
-        self.exprs = exprs
+class SuperExpr(NonTerminalExpr):
+    def __init__(self, exprs: typing.List[Expr]):
+        super().__init__(exprs)
 
     def __repr__(self):
         return (
@@ -301,11 +361,13 @@ class SuperExpr(Expr):
     def value(self) -> Number:
         return sum(expr.value for expr in self.exprs)
 
+
 def consume(c: str, current_token: typing.Optional[Token]) -> TokenPair:
     if current_token:
         return current_token.consume(c)
     else:
         return None, Token.from_char(c)
+
 
 def tokenize(string: str):
     tokens: typing.List[Token] = []
@@ -320,6 +382,7 @@ def tokenize(string: str):
         tokens.append(current_token)
 
     return tokens
+
 
 def parse_tokens(tokens: typing.List[Token]):
     exprs: typing.List[typing.Union[Expr, Operator]] = []
@@ -343,35 +406,37 @@ def parse_tokens(tokens: typing.List[Token]):
             exprs.append(token)
         else:
             raise ValueError(f"Can't handle token of type: {type(token)}")
-    
+
     # Collapse operators with their operands, in order of precedence
     p = 1
     while p <= Operator.MAX_PRECEDENCE:
         i = 1
         while i < len(exprs) - 1:
-            left, op, right = exprs[i - 1:i + 2]
+            left, op, right = exprs[i - 1 : i + 2]
             if isinstance(op, Operator) and op.precedence == p:
                 try:
                     assert isinstance(left, Expr)
                     assert isinstance(right, Expr)
-                    
+
                     exprs = [
-                        *exprs[:i - 1],
+                        *exprs[: i - 1],
                         OpExpr.from_token(left, op, right),
-                        *exprs[i + 2:]
+                        *exprs[i + 2 :],
                     ]
                 except AssertionError:
                     raise ValueError(
                         f"Invalid operands for {op.token}: {left} and {right}."
                     )
 
-                i -= 2
+                i -= 1
             i += 1
         p += 1
 
     return exprs
 
-def parse(string: str):
-    return parse_tokens(tokenize(string))[0] 
 
-print(repr(parse("2d12 k3+4*(3 / 2)")))
+def parse(string: str):
+    return parse_tokens(tokenize(string))
+
+
+print(parse("2d12 k3+4*(3 / 2) 2d4")[1].roll_info())
